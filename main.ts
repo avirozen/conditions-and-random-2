848ee77b6ce92a5b912223693fa358b93bafc96c
avirozen <117524971+avirozen@users.noreply.github.com>
2023-11-27 10:50:15 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,33 +3,14 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(400)
     scissors_paper_rock = randint(1, 3)
     basic.showNumber(scissors_paper_rock)
-    if (scissors_paper_rock == 1) {
-        basic.showLeds(`
-            # # . . #
-            # # . # .
-            . . # . .
-            # # . # .
-            # # . . #
-            `)
-    } else if (scissors_paper_rock == 2) {
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-    } else if (scissors_paper_rock == 3) {
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # # # # #
-            . # # # .
-            . . # . .
-            `)
-    } else {
-    	
-    }
 })
 let scissors_paper_rock = 0
 scissors_paper_rock = 0
+let counter = 0
+basic.showLeds(`
+    . . # . .
+    . # . . .
+    # # # # #
+    . # . . .
+    . . # . .
+    `)
